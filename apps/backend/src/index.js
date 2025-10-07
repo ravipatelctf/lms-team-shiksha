8ffa755b0b28ca6d644faa96997650b2ab2883b1
Ravi Shankar Patel <ravipatelctf@gmail.com>
2025-10-07 18:32:38 +0530

    fix: fix vercel.json

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -4,7 +4,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Home route
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.status(200).json({"message": "Welcome to the home page of lms-team-shiksha backend"})
 })
 
